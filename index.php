617995ac362c47bf038c5237abedbbf6f6da1840
keevj <jhonatan72700606@gmail.com>
2022-01-27 01:10:12 -0400

    Update 27/01/2022 - 01:16 AM

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/script.js"></script>
+  <title>Joker | System</title>
+</head>
+<body>
+  <div class="loader">
+    <img src="images/preloader/load-gif.gif" alt="">
+  </div>
+  <div id="particles-js">
+    <header>
+      <main>
+        <ul>
+          <li><a href="#">Inicio</a></li>
+          <li><a href="#">Cursos</a></li>
+        </ul>
+      </main>
+    </header>
+  </div>
+  <!-- Efecto particle -->
+  <script src="js/particle/particles.js"></script>
+  <script src="js/particle/app.js"></script>
+  <!-- Aqui termina el preloader -->
+  <script src="js/preloader.js"></script>
+</body>
+</html>
