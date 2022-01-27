09f3abbd9b8e4de245295c15573e5d69d7b8f258
keevj <jhonatan72700606@gmail.com>
2022-01-27 16:51:55 -0400

    Update 27/01/2022 - 16:51 PM

--- index.php
+++ index.php
@@ -6,26 +6,36 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style.css">
   <script src="js/script.js"></script>
+  <script src="js/menu.js"></script>
+  <script src="https://kit.fontawesome.com/9b6a0034ad.js" crossorigin="anonymous"></script>
+  <link rel="shortcut icon" href="images/icon/j.ico" type="image/x-icon">
   <title>Joker | System</title>
 </head>
 <body>
   <div class="loader">
     <img src="images/preloader/load-gif.gif" alt="">
   </div>
-  <div id="particles-js">
-    <header>
-      <main>
-        <ul>
-          <li><a href="#">Inicio</a></li>
-          <li><a href="#">Cursos</a></li>
-        </ul>
-      </main>
-    </header>
+  <div class="container">
+    <div id="particles-js" class="particles">
+      <header class="container-menu">
+        <nav class="navegation">
+          <a href="index.php" class="logotipo">Joker</a>
+          <ul class="menu_items">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Cursos</a></li>
+            <li><a href="#">Profesores</a></li>
+          </ul>
+          <span class="icon_menu">
+            <i class="fa fa-bars"></i>
+          </span>
+        </nav>
+      </header>
+    </div>
+    <script src="js/particle/particles.js"></script>
+    <script src="js/particle/app.js"></script>
+    <h1>fin</h1>
+    <!-- Aqui termina el preloader -->
+    <script src="js/preloader.js"></script>
   </div>
-  <!-- Efecto particle -->
-  <script src="js/particle/particles.js"></script>
-  <script src="js/particle/app.js"></script>
-  <!-- Aqui termina el preloader -->
-  <script src="js/preloader.js"></script>
 </body>
 </html>
